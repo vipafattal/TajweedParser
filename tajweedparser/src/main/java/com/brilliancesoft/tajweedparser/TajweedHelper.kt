ab8186a532a5228d365f129f55098ad6485b1a93
vipafattal <vipafattal@gmail.com>
2020-10-29 11:08:03 +0300

    Getting aya without colors and metas now supported

--- tajweedparser/src/main/java/com/brilliancesoft/tajweedparser/TajweedHelper.kt
+++ tajweedparser/src/main/java/com/brilliancesoft/tajweedparser/TajweedHelper.kt
@@ -20,6 +20,10 @@ object TajweedHelper {
         return applyTajweedColors(ayahSplits)
     }
 
+    fun getAyahWithoutMetas(rawAyah: String): String {
+        return rawAyah.replace("[]\\[$tajweedMetas:0-9]".toRegex(), "")
+    }
+
     private fun getAyahSplits(rawAyah: String): List<String> {
         val ayah = rawAyah.replace("[\\[0-9:]".toRegex(), "")
         val splits = ayah.splitWithDelimiter("(?<=[\\]\\[$tajweedMetas])|(?=[\\]\\[$tajweedMetas])")
@@ -49,31 +53,31 @@ object TajweedHelper {
     }
 
     private fun String.splitWithDelimiter(input: CharSequence) =
-            this.split(Regex("(?<=[$input])|(?=[$input])"))
+        this.split(Regex("(?<=[$input])|(?=[$input])"))
 
 
     private fun metaToColor(meta: Char): String =
-            tajweedMetaColors.run {
-                val color: String = when (meta) {
-                    'h', 'l', 's' -> hsl
-                    'n' -> madda_normal
-                    'p' -> madda_permissible
-                    'm' -> madda_necesssary
-                    'q' -> qalaqah
-                    'o' -> madda_obligatory
-                    'c' -> ikhafa_shafawi
-                    'f' -> ikhafa
-                    'w' -> idgham_shafawi
-                    'i' -> iqlab
-                    'a' -> idgham_with_ghunnah
-                    'u' -> idgham_without_ghunnah
-                    'd'-> idgham_mutajanisayn
-                    'b' -> idgham_mutaqaribayn
-                    'g' -> ghunnah
-                    else -> throw IllegalArgumentException("Unknown meta char: $meta")
-                }
-                return color
+        tajweedMetaColors.run {
+            val color: String = when (meta) {
+                'h', 'l', 's' -> hsl
+                'n' -> madda_normal
+                'p' -> madda_permissible
+                'm' -> madda_necesssary
+                'q' -> qalaqah
+                'o' -> madda_obligatory
+                'c' -> ikhafa_shafawi
+                'f' -> ikhafa
+                'w' -> idgham_shafawi
+                'i' -> iqlab
+                'a' -> idgham_with_ghunnah
+                'u' -> idgham_without_ghunnah
+                'd' -> idgham_mutajanisayn
+                'b' -> idgham_mutaqaribayn
+                'g' -> ghunnah
+                else -> throw IllegalArgumentException("Unknown meta char: $meta")
             }
+            return color
+        }
 
 
     private fun SpannableStringBuilder.setSpanOnAya(ayahText: String, color: String) {
@@ -83,10 +87,10 @@ object TajweedHelper {
         val end = this.length
 
         setSpan(
-                ForegroundColorSpan(Color.parseColor(color)),
-                start,
-                end,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            ForegroundColorSpan(Color.parseColor(color)),
+            start,
+            end,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
     }
 }
